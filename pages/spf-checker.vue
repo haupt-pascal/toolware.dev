7210e000b1749c9f163a182af99b1ba4c567a044
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2023-11-21 08:53:29 +0100

    added texts to spf checker

--- pages/spf-checker.vue
+++ pages/spf-checker.vue
@@ -17,6 +17,44 @@
       </p>
     </div>
   </div>
+  <div class="input-container info-container">
+    <h2>What is an SPF-Record?</h2>
+    <p class="along">
+      SPF stands for Sender Policy Framework. It is a DNS TXT record that
+      specifies which IP addresses and/or hostnames are allowed to send emails
+      from a specific domain. SPF records are used to prevent spammers from
+      sending messages with forged From addresses at that domain.
+    </p>
+    <h3>
+      What is an good SPF-Record in general?
+    </h3>
+    <span class="along">
+      v=spf1 a mx ~all
+    </span>
+    <h3>
+      What is an good SPF-Record for Google Workspace?
+    </h3>
+    <span class="along">
+      v=spf1 include:_spf.google.com ~all
+    </span>
+    <h3>
+      What is an good SPF-Record for Microsoft 365?
+    </h3>
+    <span class="along">
+      v=spf1 include:spf.protection.outlook.com -all
+    </span>
+  </div>
+
+  <div class="input-container info-container">
+    <li class="along"><p>A</p><p class="along">Stands for including every A-Tag inside your dns, also subdomains</p></li>
+    <div class="divider">&nbsp;</div>
+    <li class="along"><p>MX</p><p class="along">Stands for including every MX-Tag inside your dns</p></li>
+    <div class="divider">&nbsp;</div>
+    <li class="along"><p>~all</p><p class="along">Stands for softfail, which means that mails outside an included mailserver will be delivered into spam</p></li>
+    <div class="divider">&nbsp;</div>
+    <li class="along"><p>-all</p><p class="along">Stands for hardfail, which means that mails outside an included mailserver will not be delivered</p></li>
+
+  </div>
   <Footer />
 </template>
 

8e80a22ec7f85885ab497a7c45521c57f74d08a4
Pascal Haupt <pascal.haupt@profihost.ag>
2024-03-28 15:40:04 +0100

    command injection prevention

--- server/api/ping.ts
+++ server/api/ping.ts
@@ -2,19 +2,23 @@ import childProcess from 'child_process';
 
 export default defineEventHandler(async (process) => {
     const query = getQuery(process);
-    const commandOutput = childProcess.execSync(`ping -c 5 ${query.url}`).toString();
-    // prevent command injection
+    const input = query.url as string;
 
-    if (commandOutput.includes("&" || "|" || ";" || "&&" || "||" || "`" || "$" || "<" || ">" || "(" || ")")) {
+    if (!input) {
+        return "Please provide a URL";
+    }
+
+    if (input.includes("&" || "|" || ";" || "&&" || "||" || "`" || "$" || "<" || ">" || "(" || ")")) {
         return "You fucked up the command!";
+    } else {
+        const commandOutput = childProcess.execSync(`ping -c 5 ${query.url}`).toString();
+    
+        if (commandOutput === "") {
+            return "No ping record found";
+        }
+
+        const lines = commandOutput.split('\n');
+
+        return lines;
     }
-
-
-    if (commandOutput === "") {
-        return "No ping record found";
-    }
-
-    const lines = commandOutput.split('\n');
-
-    return lines;
 });
